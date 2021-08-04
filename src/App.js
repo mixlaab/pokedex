8025acc5ed141a5924c93553e21d4fa794091723
Víctor Medrano <vdejesus@Victors-MacBook-Pro.local>
2021-08-04 15:15:58 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pokedex: [],
+    };
+  }
+
+  componentDidMount() {
+    /*
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => console.log(users));
+      */
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=9")
+      .then((response) => response.json())
+      .then((pokedex) => this.setState({ pokedex: pokedex.results }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.pokedex.map((pokemon, index) => (
+          <>
+            <h1 key={index}>{pokemon.name}</h1>
+            <a href={pokemon.url} key={index}>
+              {pokemon.url}
+            </a>
+          </>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
